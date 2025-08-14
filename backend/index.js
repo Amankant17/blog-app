38fb6bb7e8cc55350cc6dbe737801f182e7cb16b
“[Aman <amankant565@gmail.com>
2025-08-14 15:13:49 +0530

    fixed backend port

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const router = require("./routes")
 app.use("/api", router);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 app.listen(PORT, () => {
   console.log("Server is up at port", PORT)
 })
