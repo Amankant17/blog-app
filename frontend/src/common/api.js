d0c2fd8fdcbce8a4a555e0415fd3c1987756f8d7
“[Aman <amankant565@gmail.com>
2025-08-14 15:32:04 +0530

    Changed the baseURL of frontend

--- frontend/src/common/api.js
+++ frontend/src/common/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://blog-app-2rbn.onrender.com/api',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
